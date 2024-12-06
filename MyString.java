e81ff9ba57398031384edf5d97cfa7324d513f17
Ron Dahan <rondahan@Rons-MacBook-Pro.local>
2024-12-06 09:56:34 +0200

    Finished MyString, need to do Scrabble

--- MyString.java
+++ MyString.java
@@ -1,14 +1,18 @@
+
+
 /**
  * A library of string functions.
  */
 public class MyString {
     public static void main(String args[]) {
         String hello = "hello";
-        System.out.println(countChar(hello, 'h'));
-        System.out.println(countChar(hello, 'l'));
-        System.out.println(countChar(hello, 'z'));
+        System.out.println(countChar(hello, 'h')); // 1
+        System.out.println(countChar(hello, 'l')); // 2
+        System.out.println(countChar(hello, 'z')); // 0
         System.out.println(spacedString(hello));
-        //// Put your other tests here.
+        System.out.println(subsetOf("c", "space"));
+        System.out.println(remove("committee","meet"));
+                //// Put your other tests here.
     }
 
     /**
@@ -20,8 +24,13 @@ public class MyString {
      * @return the number of times c appears in str
      */
     public static int countChar(String str, char ch) {
-        //// Replace the following statement with your code
-        return 0;
+     int count=0;
+     for (int i = 0 ; i<str.length();i++){
+       if (str.charAt(i) == ch){
+        count++;
+       }
+     }
+        return count;
     }
 
     /** Returns true if str1 is a subset string str2, false otherwise
@@ -35,8 +44,26 @@ public class MyString {
      * @param str2 - a string
      * @return true is str1 is a subset of str2, false otherwise
      */
-    public static boolean subsetOf(String str1, String str2) {
-         //// Replace the following statement with your code
+    public static boolean subsetOf(String str2, String str1) { // finished subsetOf
+        if (str2.length() == 0){
+            return true;
+        }
+        if (str2.length() > str1.length()){
+            return false;
+        }
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) == str2.charAt(0)) { // if the first letter of str2 is in str1
+                int j = 0;
+                str1=remove(str1, "" + str1.charAt(j));
+                str2=remove(str2, "" + str2.charAt(j));
+                while (j < str2.length()  && str1.charAt(i+j) == str2.charAt(j)) { // checks if the rest of the letters are the same
+                    j++;
+                }
+                if (j == str2.length() || str2.length()==1 ) { // if all the letters are the same
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -49,8 +76,12 @@ public class MyString {
      * @return a string consisting of the characters of str, separated by spaces.
      */
     public static String spacedString(String str) {
-        //// Replace the following statement with your code
-        return null;
+     String newStr = "";
+     for (int i=0 ; i<str.length();i++){
+        newStr=newStr + str.charAt(i);
+        newStr=newStr + " ";
+     }
+     return newStr;
     }
   
     /**
@@ -64,22 +95,31 @@ public class MyString {
      * @return a randomly generated string, consisting of 'n' lowercase letters
      */
     public static String randomStringOfLetters(int n) {
-        //// Replace the following statement with your code
-        return null;
+        String word ="";
+    for ( int i = 0 ; i<n; i++){
+    char temp = (char) (Math.random() * 26 + 97);
+    word = word + temp;
+    }
+        return word;
     }
 
     /**
      * Returns a string consisting of the string str1, minus all the characters in the
      * string str2. Assumes (without checking) that str2 is a subset of str1.
-     * Example: remove("meet","committee") returns "comit" 
+     * Example: remove("committee","meet") returns "comit" 
      * 
      * @param str1 - a string
      * @param str2 - a string
      * @return a string consisting of str1 minus all the characters of str2
      */
     public static String remove(String str1, String str2) {
-       //// Replace the following statement with your code
-        return null;
+    String temp = str1;
+        for (int i=0; i<str2.length();i++){
+        int index = temp.indexOf(str2.charAt(i));
+        if (index == -1) return temp;
+        temp = temp.substring(0, index ) + temp.substring(index+1 , temp.length());
+         }
+    return temp;
     }
 
     /**
